ce770ac53147b3396c8f1c263e1105cf01e35d4b
Ulrich Fischer <ulrich.fischer@hexagongeospatial.com>
2016-10-04 09:11:33 +0200

    Element resize

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function () {
+  gulp.start('watch');
+});
+
+gulp.task('compile-typescript', function () {
+    return gulp.src('app/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(ts({
+            target: 'ES5',
+            out: 'mapp.ple.js'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile-less', function () {
+    return gulp.src('styles/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/**/*.ts', ['compile-typescript']);
+  gulp.watch('styles/*.less', ['compile-less']);
+});
